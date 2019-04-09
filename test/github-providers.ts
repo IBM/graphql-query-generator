f0450925ae88bd9ba7463237963ae11897db7adc
Alan Cha <Alan.cha1@ibm.com>
2019-04-09 13:32:25 -0400

    Added more providers

--- test/github-providers.ts
+++ test/github-providers.ts
@@ -35,19 +35,38 @@ function getRandomRepositoryName () {
   return 'oasgraph'
 }
 
+function getRandomValueFromArray<T> (array: T[]) : T {
+  return array
+    ? array[Math.floor(Math.random() * array.length)]
+    : undefined
+}
+
 export const GITHUB_PROVIDERS = {
   '*__*__first': 10,
   '*__codeOfConduct__key': 'citizen_code_of_conduct', // or 'contributor_covenant'
   '*__gist__name': 'index.html',
   '*__*__number': 1,
   '*__search__query': 'test',
+  '*__search__type': getRandomValueFromArray(['ISSUE', 'REPOSITORY', 'USER']),
   '*__repository__name': getRandomRepositoryName,
+  '*__repository__owner': getRandomUserLogin,
   '*__marketplaceListing__slug': 'wakatime', // see 'https://github.com/marketplace',
   '*__marketplaceCategory__slug': getRandomMarketplaceSlug,
   '*__user__login': getRandomUserLogin,
-  '*__repository__owner': getRandomUserLogin,
   '*__license__key': getRandomLicenseKey,
   '*__organization__login': 'Facebook',
   '*__topic__name': 'test',
-  '*__ref__qualifiedName': '/ref/head/'
+  '*__ref__qualifiedName': '/ref/head/',
+  
+  '*__node__id': 'MDEwOlJlcG9zaXRvcnk1MjE2OTEz',
+  '*__nodes__ids': ['MDEwOlJlcG9zaXRvcnk1MjE2OTEz'],
+  '*__repositoryOwner__login': getRandomUserLogin,
+  '*__resource__url': 'https://github.com/strongloop/oasgraph',
+  '*__project__number': 1,
+  'Organization__team__slug': 'oasgraph-maintainers',
+  '*__issue__number': 1,
+  '*__issueOrPullRequest__number': 1,
+  'Repository__label__name': getRandomValueFromArray(['bug', 'duplicate', 'enhancement', 'good first issue', 'help wanted', 'invalid', 'question']),
+  '*__pullRequest__number': 1,
+  '*__release__tagname': 'v0.14.0'
 }
