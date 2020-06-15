fc3d982c0b643b7727032f3414d45a25a477aec6
Alan Cha <Alan.cha1@ibm.com>
2020-06-15 10:55:51 -0400

    Add Prettier config

--- lib/provide-variables.d.ts
+++ lib/provide-variables.d.ts
@@ -1,36 +1,16 @@
 import { GraphQLNamedType } from "graphql";
 import { Configuration } from "./generate-query";
 declare type Variables = {
-  [varName: string]: any;
+    [varName: string]: any;
 };
-export declare type ProviderFunction = (
-  variables: Variables,
-  argType?: GraphQLNamedType
-) =>
-  | any
-  | {
-      [argumentName: string]: any;
-    };
+export declare type ProviderFunction = (variables: Variables, argType?: GraphQLNamedType) => any | {
+    [argumentName: string]: any;
+};
 export declare type ProviderMap = {
-  [varNameQuery: string]:
-    | string
-    | boolean
-    | number
-    | Date
-    | Object
-    | Array<any>
-    | ProviderFunction;
+    [varNameQuery: string]: string | boolean | number | Date | Object | Array<any> | ProviderFunction;
 };
-export declare function matchVarName(
-  query: string,
-  candidates: string[]
-): string;
+export declare function matchVarName(query: string, candidates: string[]): string;
 export declare function getRandomEnum(type: GraphQLNamedType): string;
 export declare function isEnumType(type: GraphQLNamedType): boolean;
-export declare function getProviderValue(
-  varName: string,
-  config: Configuration,
-  providedValues: Variables,
-  argType?: GraphQLNamedType
-): any;
+export declare function getProviderValue(varName: string, config: Configuration, providedValues: Variables, argType?: GraphQLNamedType): any;
 export {};
